b03b718d2e133805f60c1368a27b3b3533f5bab8
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-02 05:47:09 +0200

    Adds sendMail() method, changes index() method in ErrorController.

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -1,20 +1,37 @@
 <?php
-/**
- * Created by xalbert.einsteinx
- * https://www.einsteinium.pro
- * Date: 13.05.2016
- * Time: 17:15
- */
-
 namespace bl\cms\error\controllers;
 
-
+use yii\helpers\Url;
 use yii\web\Controller;
 
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
 class ErrorController extends Controller
 {
-    public function actionIndex() {
+    /**
+     * @return string
+     */
+    public function actionIndex()
+    {
+
         $exception = \Yii::$app->errorHandler->exception;
+
+        if (\Yii::$app->getModule('error')->sendMail) {
+            $this->sendMail($exception);
+        }
+
         return $this->render('index', ['exception' => $exception]);
     }
+
+    private function sendMail($exception)
+    {
+        \Yii::$app->errorMailer->compose('error-mail', ['exception' => $exception])
+            ->setFrom([\Yii::$app->getModule('error')->webmasterEmail => \Yii::$app->name ?? Url::to(['/'], true)])
+            ->setTo('xalbert.einsteinx@gmail.com')
+            ->setSubject($exception->getMessage())
+            ->send();
+
+    }
+
 }
