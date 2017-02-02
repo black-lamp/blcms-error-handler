8ae2baf68feff604cdb161e703862697fa9a81a0
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-02 05:46:20 +0200

    Adds translations.

--- messages/ru-RU/main.php
+++ messages/ru-RU/main.php
@@ -2,5 +2,8 @@
 return [
     'Unknown error' => 'Неизвестная ошибка',
     'Home' => 'На главную',
-    'Back' => 'Назад'
+    'Back' => 'Назад',
+    'Unfortunately, this page is not available. Error information has been sent to the site administrator.' =>
+        'К сожалению, эта страница сейчас не доступна. Информация об ошибке уже отправлена администратору сайта. Приносим свои извинения.',
+    'You can contact the site administrator for more information at' => 'Вы можете связаться с администратором сайта для детальной информации'
 ];
--- messages/uk/main.php
+++ messages/uk/main.php
@@ -2,5 +2,8 @@
 return [
     'Unknown error' => 'Невідома помилка',
     'Home' => 'На головну',
-    'Back' => 'Назад'
+    'Back' => 'Назад',
+    'Unfortunately, this page is not available. Error information has been sent to the site administrator.' =>
+        'На жаль, ця сторінка зараз не доступна. Інформація про помилку вже відправлена адміністратору сайту. Приносимо свої вибачення.',
+    'You can contact the site administrator for more information at' => 'Ви можете зв\'язатися з адміністратором сайту для детальної інформації'
 ];
