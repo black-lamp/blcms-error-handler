a18d5322a6be9b60e6002f6c2b517845a99f5d35
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-09 15:21:00 +0200

    Adds translations.

--- messages/uk/main.php
+++ messages/uk/main.php
@@ -4,6 +4,8 @@ return [
     'Home' => 'На головну',
     'Back' => 'Назад',
     'Unfortunately, this page is not available. Error information has been sent to the site administrator.' =>
-        'На жаль, ця сторінка зараз не доступна. Інформація про помилку вже відправлена адміністратору сайту. Приносимо свої вибачення.',
-    'You can contact the site administrator for more information at' => 'Ви можете зв\'язатися з адміністратором сайту для детальної інформації'
+        'На жаль, ця сторінка зараз не доступна. Приносимо свої вибачення.',
+    'You can contact the site administrator for more information at' => 'Ви можете зв\'язатися з адміністратором сайту для детальної інформації',
+    'Error report' => 'Повідомити про помилку',
+    'Your error report has sent successfully' => 'Ваше повідомлення про помилку надіслано. Дякуємо.'
 ];
