8db1c807e8c02045b6ffe31030fced4a6c9b3558
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-10 14:38:25 +0200

    Changes error page.

--- views/error/index.php
+++ views/error/index.php
@@ -23,59 +23,64 @@ if ($exception) {
         <h1 class="error-message"><?= Yii::t('modules/errorhandler/main', 'Unknown error'); ?></h1><br>
     <?php endif; ?>
 
+    <!--ERROR MESSAGE-->
+    <p class="text-center">
+        <?= Yii::t('modules/errorhandler/main', \Yii::$app->getModule('error')->errorMessage); ?>
+    </p>
 
-    <?php if (\Yii::$app->getModule('error')->sendMailAutomatically) : ?>
-        <!--ERROR MESSAGE-->
+    <!--WEBMASTER EMAIL-->
+    <?php if (!empty(\Yii::$app->getModule('error')->webmasterEmail)): ?>
         <p class="text-center">
-            <?= Yii::t('modules/errorhandler/main', \Yii::$app->getModule('error')->errorMessage); ?>
+            <?= Yii::t('modules/errorhandler/main', 'You can contact the site administrator for more information at'); ?>
+            <a href="mailto:<?= \Yii::$app->getModule('error')->webmasterEmail; ?>">
+                <?= \Yii::$app->getModule('error')->webmasterEmail; ?>
+            </a>
         </p>
     <?php endif; ?>
-    <div class="row">
-        <!--WEBMASTER EMAIL-->
-        <?php if (!empty(\Yii::$app->getModule('error')->webmasterEmail)): ?>
-            <p class="text-center">
-                <?= Yii::t('modules/errorhandler/main', 'You can contact the site administrator for more information at'); ?>
-                <a href="mailto:<?= \Yii::$app->getModule('error')->webmasterEmail; ?>">
-                    <?= \Yii::$app->getModule('error')->webmasterEmail; ?>
-                </a>
-            </p>
-        <?php endif; ?>
-    </div>
-
 
     <div class="row">
+        <div class="col-md-4 col-md-offset-4">
 
-        <?php if (\Yii::$app->getModule('error')->enableUserMessageSending) : ?>
-            <!--ERROR REPORTING-->
-            <?php $errorForm = ActiveForm::begin([
-                'action' => Url::to(['/error/error/report-error'])
-            ]); ?>
+            <?php if (\Yii::$app->getModule('error')->enableUserMessageSending) : ?>
+                <div class="row">
 
-            <div class="col-md-4 col-md-offset-4">
-                <?= $errorForm->field($errorMailForm, 'url')
-                    ->hiddenInput(['value' => Url::to(\Yii::$app->request->url, true)])->label(false); ?>
-                <?= $errorForm->field($errorMailForm, 'message')
-                    ->hiddenInput(['value' => $exception->getMessage()])->label(false); ?>
-                <?= $errorForm->field($errorMailForm, 'email')
-                    ->textInput(['placeholder' => \Yii::t('modules/errorhandler/main', 'Your email')])
-                    ->label(false); ?>
+                    <!--ERROR REPORTING-->
+                    <?php $errorForm = ActiveForm::begin([
+                        'action' => Url::to(['/error/error/report-error']),
+                    ]); ?>
+
+                    <div class="col-md-12">
+                        <?= $errorForm->field($errorMailForm, 'url')
+                            ->hiddenInput(['value' => Url::to(\Yii::$app->request->url, true)])->label(false); ?>
+                        <?= $errorForm->field($errorMailForm, 'message')
+                            ->hiddenInput(['value' => $exception->getMessage()])->label(false); ?>
+                        <?= $errorForm->field($errorMailForm, 'email')
+                            ->textInput(['placeholder' => \Yii::t('modules/errorhandler/main', 'Your email')])
+                            ->label(false); ?>
+
+                        <?= Html::submitButton(\Yii::t('modules/errorhandler/main', 'Report an error'), [
+                            'class' => 'btn btn-warning'
+                        ]); ?>
+                    </div>
+                    <?php $errorForm::end(); ?>
+                </div>
+            <?php endif; ?>
+
+            <!--BACK-->
+            <br>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php if (Yii::$app->request->getReferrer()): ?>
+                        <a class="btn btn-primary" href="<?= Yii::$app->request->getReferrer() ?>">
+                            <?= Yii::t('modules/errorhandler/main', 'Back'); ?>
+                        </a>
+                    <?php else: ?>
+                        <a class="btn btn-primary col-md-12"
+                           href="<?= Url::to(['/']); ?>"><?= Yii::t('modules/errorhandler/main', 'Home'); ?></a>
+                    <?php endif; ?>
+                </div>
             </div>
-
-            <?= Html::submitButton(\Yii::t('modules/errorhandler/main', 'Report an error'),[
-                'class' => 'btn btn-warning col-md-2 col-md-offset-5 m-b-md'
-            ]); ?>
-            <?php $errorForm::end(); ?>
-        <?php endif; ?>
-
-        <!--BACK-->
-        <?php if (Yii::$app->request->getReferrer()): ?>
-            <a class="btn btn-primary" href="<?= Yii::$app->request->getReferrer() ?>">
-                <?= Yii::t('modules/errorhandler/main', 'Back'); ?>
-            </a>
-        <?php else: ?>
-            <a class="btn btn-primary col-md-2 col-md-offset-5"
-               href="<?= Url::to(['/']);?>"><?= Yii::t('modules/errorhandler/main', 'Home'); ?></a>
-        <?php endif; ?>
+        </div>
 
     </div>
 
